adb164e271c8578d06207193c6e85eb5322428a5
Marcos Fraga <marcosjsfraga@gmail.com>
2020-07-11 13:01:57 -0300

    First commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+	Container,
+	Title,
+	ForgotPassword,
+	ForgotPasswordText,
+	CreateAccountButton,
+	CreateAccountButtonText,
+} from './styles';
+
+const SignIn: React.FC = () => {
+	const navigation = useNavigation();
+
+	return (
+		<KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
+			<ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+				<Container>
+					<Image source={logoImg} />
+					{/* This View makes the title move when keyboard appears  */}
+					<View>
+						<Title>Faça seu login</Title>
+					</View>
+
+					<Input name="email" icon="mail" placeholder="E-mail" />
+					<Input name="password" icon="lock" placeholder="Senha" />
+
+					<Button
+						onPress={() => {
+							console.log('Btn..');
+						}}
+					>
+						Entrar
+					</Button>
+
+					<ForgotPassword onPress={() => {}}>
+						<ForgotPasswordText>Esqueceu sua senha?</ForgotPasswordText>
+					</ForgotPassword>
+
+					<CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+						<Icon name="log-in" size={20} color="#ff9000" />
+						<CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+					</CreateAccountButton>
+				</Container>
+			</ScrollView>
+		</KeyboardAvoidingView>
+	);
+};
+
+export default SignIn;
